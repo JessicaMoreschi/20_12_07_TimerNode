a5edaab92f98297cec57e0b37f3b4de206983ecc
JessicaMoreschi <71377205+JessicaMoreschi@users.noreply.github.com>
2020-12-08 12:33:46 +0100

    comment site

--- public/sketch_website.js
+++ public/sketch_website.js
@@ -1,6 +1,6 @@
-// SERVER
+// VAR SERVER
 let socket = io(); //setting server
-// SKETCH
+// VAR TIMER
 var x; //setInterval
 var countDown;
 var gap; //gap tra countDown e Now
@@ -8,56 +8,43 @@ var runningTime = 0; //secondi che scorrono
 var thisTime = 180; //secondi allo stopTimer
 var testo = 180; //variabile testo this countdown
 
-var playAllVideo = false; //bouleana legata al countdown (play/stop)
+var playAllVideo = false; //bouleana play/stop countdown
 let videoAction;
-let videoActionStart = 180; //definisci quando inizia video
-let videoActionStop = 0; //definisci quando finisce video
+let videoActionStart = 180; //inizio video Action
+let videoActionStop = 0; //fine video Action
 let videoCorner;
-let videoCornerStart = 175; //definisci quando inizia video
-let videoCornerStop = 170; //definisci quando finisce video
+let videoCornerStart = 175; //inizio video
+let videoCornerStop = 170; //fine video Corner
 let videoGoal;
-let videoGoalStart = 165; //definisci quando inizia video
-let videoGoalStop = 160; //definisci quando finisce video
+let videoGoalStart = 165; //inizio video
+let videoGoalStop = 160; //fine video Goal
 
 
-// SERVER
-socket.on("connect", newConnection); //quando mi connetto, chiama funzione newConnection
-socket.on("startTimer", startTimer); //ricezione other coundown
-socket.on("stopTimer", stopTimer);
-socket.on("resetTimer", resetTimer);
+// RICEZIONE SERVER
+socket.on("startTimer", startTimer); // StartTimer
+socket.on("stopTimer", stopTimer); // StopTimer
+socket.on("resetTimer", resetTimer); // ResetTimer
 
-function newConnection() {
-  console.log("your ID: " + socket.id) //mostra mio codice connessione
-}
 
-// SKETCH
 function setup() {
   myCanvas = createCanvas(windowWidth, windowHeight);
   myCanvas.position(0, 0);
 
-  //button start
-  var btn;
+// SETUP TIMER
+  var btn; //button start
   btn = createButton("Start Time");
   btn.position(width / 2 + 100, height / 2 - 25);
   btn.mouseClicked(startTimer);
-  //button stop
-  var btn2;
+  var btn2; //button stop
   btn2 = createButton("Stop Time");
   btn2.position(width / 2 + 100, height / 2);
   btn2.mouseClicked(stopTimer);
-  //button reset
-  var btn3;
+  var btn3; //button reset
   btn3 = createButton("Reset Time");
   btn3.position(width / 2 + 100, height / 2 + 25);
   btn3.mouseClicked(resetTimer);
 
-  //button other page
-  var btn4;
-  btn4 = createButton("Other page");
-  btn4.position(width / 2 + 100, height / 2 + 100);
-  btn4.mouseClicked(openOtherPage);
-
-  // video
+// SETUP VIDEO
   videoAction = createVideo('assets/action.mp4');
   videoAction.hide();
   videoGoal = createVideo('assets/goal.mp4');
@@ -69,17 +56,21 @@ function setup() {
 
 function draw() {
   background("255");
-  //this countdown
+
+// DISPLAY COUNTDOWN
   push()
   textSize(20);
   fill("black");
   text(testo, width / 2 + 50, height / 2 + 15);
   pop()
-  // text fine partita
   if (gap < 0) {
-    testo = "finish"
+    testo = "finish" // text fine partita
   }
-  //video
+
+//EMIT COUNTDOWN
+  socket.emit("testoOut", testo);
+
+// DISPLAY VIDEO
   if (testo < videoActionStart && testo > videoActionStop) {
     imageMode(CENTER);
     image(videoAction, 180, height / 2, 160 * 2, 100 * 2);
@@ -93,68 +84,57 @@ function draw() {
     image(videoCorner, 180, height / 2, 160 * 2, 100 * 2);
   }
 
+// PLAY/STOP VIDEO
   toggleVid(); //check funzione play/stop
-
-  //invia this countdown
-  socket.emit("testoOut", testo);
 }
 
-
+//FUNCTION PLAY-STOP-RESET TIMER
 function startTimer() {
   countDown = new Date().getTime() + (thisTime * 1000); //imposta countdown da var thisTime
-  // Update the count down every 1 second
-  x = setInterval(function() {
+  x = setInterval(function() { // Update the count down every 1 second
     var now = new Date().getTime(); // Get today's date and time
     gap = countDown - now; // Find the gap between now and the count down date
     runningTime = Math.floor((gap / 1000)); // Time calculations for seconds
     testo = runningTime //setta variabile time
   }, 1000);
-
-  playAllVideo = true;
+  playAllVideo = true; //lega video al timer
 }
-
 function stopTimer() {
   clearInterval(x); //blocca countdown
   thisTime = runningTime; //registra secondo allo stop
   testo = thisTime; //visualizza secondo allo stop
   countDown = new Date().getTime() + (thisTime * 1000); //+1000=+1s
-
-  playAllVideo = false;
+  playAllVideo = false; //lega video al timer
 }
-
 function resetTimer() {
   clearInterval(x); //blocca countdown
   thisTime = 180; //resetta countdown
   testo = thisTime; //visualizza countdown
   countDown = new Date().getTime() + (thisTime * 1000); //+1000=+1s
-
-  playAllVideo = false;
+  playAllVideo = false; //lega video al timer
 }
 
-// plays or pauses the video depending on current state
+// FUNCTION LINK VIDEO A TIMER
 function toggleVid() {
+  //stop
   if (playAllVideo == false) {
     videoAction.pause();
     videoGoal.pause();
     videoCorner.pause();
-  }
-    else if (playAllVideo == true) {
+  //play
+  } else if (playAllVideo == true) {
     if (testo < videoActionStart && testo > videoActionStop) {
-      videoAction.loop()
+      videoAction.loop() //videoAction
     };
     if ((testo < videoGoalStart && testo > videoGoalStop) ||
       (testo < videoCornerStart && testo > videoCornerStop)) {
-      videoAction.pause()
+      videoAction.pause() //videoAction
     };
     if (testo < videoGoalStart && testo > videoGoalStop) {
-      videoGoal.loop()
+      videoGoal.loop() //videoGoal
     };
     if (testo < videoCornerStart && testo > videoCornerStop) {
-      videoCorner.loop()
+      videoCorner.loop() //videoCorner
     };
   }
-}
-
-function openOtherPage(){
-  window.open('http://localhost:3000/piattaforma.html')
 }
